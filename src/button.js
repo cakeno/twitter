ca470aa32eb3c39fd335778df3b2b22fe35c056e
cakeno <camilla.noba@gmail.com>
2019-02-19 13:41:21 -0300

    show twitte ok

--- src/button.js
+++ src/button.js
@@ -1,4 +1,10 @@
 document.getElementById('btn-tweet').addEventListener('click',clickTweet);
 function clickTweet(evento){
-    document.getElementById('show-tweet').textContent 
+    let lastTweet = document.createElement('li');
+    let content = document.createTextNode(document.getElementById('msg-tweet').value);
+    lastTweet.appendChild(content);
+    document.body.appendChild(lastTweet);
+    let listTweet = document.getElementById('list-tweet');
+    listTweet.appendChild(lastTweet);
+        listTweet.insertBefore(lastTweet);
 }
