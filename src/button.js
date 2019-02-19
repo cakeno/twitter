9f12bb41f367e49380284e62325a1f758befd5fd
cakeno <camilla.noba@gmail.com>
2019-02-19 18:58:09 -0300

    versao 003 ok

--- src/button.js
+++ src/button.js
@@ -1,20 +1,19 @@
+const btnTweet= document.getElementById('btn-tweet');
 document.getElementById('btn-tweet').addEventListener('click',clickTweet);
 function clickTweet(){
-    const lastTweet = document.createElement('li');
+    const lastTweet= document.createElement('li');
     const msgTweet= document.getElementById('msg-tweet').value;
-    let content = document.createTextNode(msgTweet);
-    const listTweet = document.getElementById('list-tweet');
+    let content= document.createTextNode(msgTweet);
     lastTweet.appendChild(content);
     document.body.appendChild(lastTweet);
-    listTweet.appendChild(lastTweet);
+    document.getElementById('list-tweet').appendChild(lastTweet);
 //listTweet.insertBefore(lastTweet, listTweet[0]);
 };
 document.getElementById('msg-tweet').addEventListener('input',enableTweet);
 function enableTweet(){
-    const msgTweet= document.getElementById('msg-tweet').value.length;
-    const btnTweet = document.getElementById('btn-tweet');
-    btnTweet.disabled = false;
-    if(msgTweet === 0){
-        btnTweet.disabled = true;
+let msgTweet= document.getElementById('msg-tweet').value.length;
+    btnTweet.disabled= false;
+    if(msgTweet === 0 || msgTweet > 140){
+        btnTweet.disabled= true;
     }
 };
--- src/script.js
+++ src/script.js
@@ -1,15 +1,16 @@
 
 document.getElementById('msg-tweet').addEventListener('input',count);
 function count(){
-    let counter= document.getElementById('msg-tweet').value.length;
-    document.getElementById('charNum').innerHTML = counter;
-    if(counter > 140){
-        const btnTweet = document.getElementById('btn-tweet');
-        document.getElementById('charNum').innerHTML = 140 - counter;
-        btnTweet.disabled = true;
-    }if else (counter > 130){
-        
-    }if else(counter > 120){
-
+    let msgTweet= document.getElementById('msg-tweet').value.length;
+    document.getElementById('charNum').innerHTML= msgTweet;
+    if(msgTweet < 121){
+        document.getElementById('charNum').style.color= "black";
+    }if(msgTweet > 120){
+        document.getElementById('charNum').style.color= "green";
+    }if(msgTweet > 130 && msgTweet < 140){
+        document.getElementById('charNum').style.color= "blue";
+    }if(msgTweet > 140){
+        document.getElementById('charNum').style.color= "red";
+        document.getElementById('charNum').innerHTML= 140 - msgTweet;
     }
 };
