d358213ce5d74cdc23f5831ad74504fcfb44bf26
cakeno <camilla.noba@gmail.com>
2019-02-21 12:17:41 -0300

    time ok

--- src/button.js
+++ src/button.js
@@ -2,23 +2,22 @@ const btnTweet= document.getElementById('btn-tweet');
 document.getElementById('btn-tweet').addEventListener('click',()=>{
     hideTimelineB();
     clickTweet();
-    event.preventDefault();
+    //event.preventDefault();
 })
 function clickTweet(){
     const lastTweet= document.createElement('li');
-    let msgTweet= tweet.value;
+    let msgTweet= document.getElementById('msg-tweet').value;
     let lengthTweet= msgTweet.length;
     let today = new Date();
     let date= today.getHours()+":"+today.getMinutes();
     if(lengthTweet !== 0 || lengthTweet <= 140){
-        let dateTweet= date+' '+msgTweet;
-        lastTweet.appendChild(document.createTextNode(dateTweet));
+        lastTweet.appendChild(document.createTextNode(date+' '+msgTweet));
         document.body.appendChild(lastTweet);
-        document.getElementById('list-tweet').appendChild(lastTweet);
+        document.getElementById('list-tweet').appendChild(lastTweet).insertBefore;
         document.getElementById('charNum').textContent = 140;
         btnTweet.disabled= true;
     }
-    //document.getElementById('formTweet').reset();
+    document.getElementById('formTweet').reset();
 }
 function hideTimelineB(){
     document.getElementById('timeline-begin').style.cssText= 'display: none;';
