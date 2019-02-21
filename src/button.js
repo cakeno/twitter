728e78edb1dfb3c395cb5a31925c6b2ce31c6fc6
cakeno <camilla.noba@gmail.com>
2019-02-21 12:06:24 -0300

    em processo:bug do botao

--- src/button.js
+++ src/button.js
@@ -1,25 +1,24 @@
-
-var today = new Date();
-var date= today.getHours()+":"+today.getMinutes();
 const btnTweet= document.getElementById('btn-tweet');
 document.getElementById('btn-tweet').addEventListener('click',()=>{
-    clickTweet();
     hideTimelineB();
+    clickTweet();
+    event.preventDefault();
 })
 function clickTweet(){
     const lastTweet= document.createElement('li');
     let msgTweet= tweet.value;
     let lengthTweet= msgTweet.length;
+    let today = new Date();
+    let date= today.getHours()+":"+today.getMinutes();
     if(lengthTweet !== 0 || lengthTweet <= 140){
         let dateTweet= date+' '+msgTweet;
         lastTweet.appendChild(document.createTextNode(dateTweet));
         document.body.appendChild(lastTweet);
         document.getElementById('list-tweet').appendChild(lastTweet);
         document.getElementById('charNum').textContent = 140;
-        event.preventDefault();
         btnTweet.disabled= true;
     }
-    document.getElementById('formTweet').reset();
+    //document.getElementById('formTweet').reset();
 }
 function hideTimelineB(){
     document.getElementById('timeline-begin').style.cssText= 'display: none;';
