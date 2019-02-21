de827502110594183bcec0ca62347a0c05ed8a93
cakeno <camilla.noba@gmail.com>
2019-02-21 14:05:17 -0300

    div profile ok

--- src/button.js
+++ src/button.js
@@ -1,33 +1,33 @@
-const btnTweet= document.getElementById('btn-tweet');
-document.getElementById('btn-tweet').addEventListener('click',()=>{
-    hideTimelineB();
-    clickTweet();
+const btnTweet = document.getElementById('btn-tweet');
+document.getElementById('btn-tweet').addEventListener('click', () => {
+	hideTimelineB();
+	clickTweet();
 })
-function leftZeros(number){
-	if(number < 10){
-		newNumber= '0' + number
+function leftZeros(number) {
+	if (number < 10) {
+		newNumber = '0' + number
 		return newNumber
-}else{
-	return number
+	} else {
+		return number
 	}
 }
-function clickTweet(){
-    const lastTweet= document.createElement('li');
-    let msgTweet= document.getElementById('msg-tweet').value;
-    let lengthTweet= msgTweet.length;
-    let today = new Date();
-    let hour= today.getHours();
-    let min= today.getMinutes();
-    let  time= leftZeros(hour) +":"+leftZeros(min);
-    if(lengthTweet !== 0 || lengthTweet <= 140){
-        lastTweet.appendChild(document.createTextNode(time+' '+msgTweet));
-        document.body.appendChild(lastTweet);
-        document.getElementById('list-tweet').appendChild(lastTweet).insertBefore;
-        document.getElementById('charNum').textContent = 140;
-        btnTweet.disabled= true;
-    }
-    document.getElementById('formTweet').reset();
+function clickTweet() {
+	const lastTweet = document.createElement('li');
+	let msgTweet = document.getElementById('msg-tweet').value;
+	let lengthTweet = msgTweet.length;
+	let today = new Date();
+	let hour = today.getHours();
+	let min = today.getMinutes();
+	let time = leftZeros(hour) + ":" + leftZeros(min);
+	if (lengthTweet !== 0 || lengthTweet <= 140) {
+		lastTweet.appendChild(document.createTextNode(time + ' ' + msgTweet));
+		document.body.appendChild(lastTweet);
+		document.getElementById('list-tweet').appendChild(lastTweet).insertBefore;
+		document.getElementById('charNum').textContent = 140;
+		btnTweet.disabled = true;
+	}
+	document.getElementById('formTweet').reset();
 }
-function hideTimelineB(){
-    document.getElementById('timeline-begin').style.cssText= 'display: none;';
+function hideTimelineB() {
+	document.getElementById('timeline-begin').style.cssText = 'display: none;';
 }
--- src/script.js
+++ src/script.js
@@ -1,41 +1,33 @@
-function leftZeros(number){
-	if(number < 10){
-			newNumber= '0' + number
-			return newNumber
-	}else{
-		return number
+function enableTweet() {
+	let lengthTweet = document.getElementById('msg-tweet').value.length;
+	if (lengthTweet === 0 || lengthTweet > 140) {
+		btnTweet.disabled = true;
+	} else {
+		btnTweet.disabled = false;
 	}
 }
-function enableTweet(){
-	let lengthTweet= document.getElementById('msg-tweet').value.length;
-	if(lengthTweet === 0 || lengthTweet > 140){
-		btnTweet.disabled= true;
-	}else{
-		btnTweet.disabled= false;
-	}
-}
-function count(){
-  let lengthTweet= document.getElementById('msg-tweet').value.length;
-  	document.getElementById('charNum').textContent = 140 - lengthTweet;
-	if(lengthTweet < 120){
-		document.getElementById('p-msg-tweet').style.color= "#29A3EF";
-	}if(lengthTweet > 120){
-		document.getElementById('p-msg-tweet').style.color= "#FDA2B1";
-	}if(lengthTweet > 130){
-		document.getElementById('p-msg-tweet').style.color= "orange";
-	}if(lengthTweet > 140){
-		document.getElementById('p-msg-tweet').style.color= "#FC0D2C";
+function count() {
+	let lengthTweet = document.getElementById('msg-tweet').value.length;
+	document.getElementById('charNum').textContent = 140 - lengthTweet;
+	if (lengthTweet < 120) {
+		document.getElementById('p-msg-tweet').style.color = "#29A3EF";
+	} if (lengthTweet > 120) {
+		document.getElementById('p-msg-tweet').style.color = "#FDA2B1";
+	} if (lengthTweet > 130) {
+		document.getElementById('p-msg-tweet').style.color = "orange";
+	} if (lengthTweet > 140) {
+		document.getElementById('p-msg-tweet').style.color = "#FC0D2C";
 	};
 }
-function autosize(){
-  let el = this;
-  setTimeout(function(){
-    el.style.cssText = 'height:auto; padding:0';
-    el.style.cssText = 'height:' + el.scrollHeight + 'px';
-},0);
+function autosize() {
+	let el = this;
+	setTimeout(function () {
+		el.style.cssText = 'height:auto; padding:0';
+		el.style.cssText = 'height:' + el.scrollHeight + 'px';
+	}, 0);
 }
-document.getElementById('msg-tweet').addEventListener('keydown',autosize)
-document.getElementById('msg-tweet').addEventListener('input',()=>{
+document.getElementById('msg-tweet').addEventListener('keydown', autosize)
+document.getElementById('msg-tweet').addEventListener('input', () => {
 	count();
 	enableTweet();
 })
